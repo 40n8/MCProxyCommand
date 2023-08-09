462f8fdf9d66138ffc5c5011cbb654bd3f76391d
GeraldTM <99144289+GeraldTM@users.noreply.github.com>
2023-08-08 22:39:56 -0400

    begin forge port, update gradle

---
+++ forge-mod/src/main/java/io/github/geraldtm/ProxyCommandMod.java
@@ -0,0 +1,21 @@
+package io.github.geraldtm;
+
+import com.mojang.brigadier.context.CommandContext;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod("proxycommand")
+public class ProxyCommandMod {
+    public ProxyCommandMod() {
+        
+    }
+
+    private static registerCommand(){
+
+    }
+
+    private static int sendMessage() {
+        
+    }
+
+}
